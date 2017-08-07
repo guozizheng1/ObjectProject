552b1def4ef51f0745bfed364788c2970a829561
gzzwinner <gzzwinner@163.com>
2017-08-07 22:53:56 +0800

    code block update

--- src/com/imooc/animal/Cat.java
+++ src/com/imooc/animal/Cat.java
@@ -14,11 +14,31 @@ public class Cat {
 	private double weight;//体重 默认值为0.0
 	private String speices;//品种 默认值为null
 	//表示一个静态的 静态成员 类成员
+	//static+静态属性 -->>静态属性、类属性
 	public static int price;//价格
 	
+	static{
+		System.out.println("我是静态代码块1");
+	}//静态代码块
+	
 	public Cat(){
+		//static+局部变量是不存在的
+		//static int temp =14;
 		System.out.println("我是宠物猫~~");
 	}
+	
+	{
+		name = "兽兽";
+		price = 12;
+		System.out.println("我是构造代码块1");
+	}//构造代码块
+	
+	static{
+		Cat tmp = new Cat();
+		tmp.name = "";
+		price = 800;
+		System.out.println("我是静态代码块2");
+	}//静态代码块
 	
 	public Cat(int month){
 		//this.month = month;
@@ -73,15 +93,30 @@ public class Cat {
 	//方法：跑动、吃东西
 	//跑动的方法
 	public void run() {
-		this.eat();
-		System.out.println("小猫快跑");
+		eat();
+		this.name = "妞妞";
+		this.price = 20;
+		System.out.println("售价为"+Cat.price+"的"+this.name+"小猫快跑");
 	}
 	
 	public void run(String name) {
+		{
+			System.out.println("我是普通代码块1");
+		}//普通代码块1
 		System.out.println(name + "小猫快跑");
+		{
+			System.out.println("我是普通代码块2");
+		}//普通代码块2
 	}
 	//吃东西的方法
-	public void eat() {
+	//static+方法--》类方法、静态方法
+	public static void eat() {
+		//静态方法当中不能直接访问同一个类中的非静态成员，只能直接调用同一个类中的静态成员
+		//如果非要调用，只有通过对象实例化的方法，对象名.成员/方法 的方式来调用访问非静态成员
+		Cat temp = new Cat();
+		temp.run();
+		temp.name = "小胖";//静态方法中不能使用this关键字
+		temp.name = "";//
 		System.out.println("小猫吃鱼");
 	}
 
--- src/com/imooc/test/Test.java
+++ src/com/imooc/test/Test.java
@@ -8,22 +8,28 @@ public class Test {
 
 	public static void main(String[] args) {
 		Cat one = new Cat();
-		one.setName("花花");
-		one.setMonth(2);
-		one.setSpeices("英国短毛猫");
-		one.price = 1000;
-		Cat.price = 2000;
-//		Cat two = new Cat();
-//		two.setName("凯特");
-//		two.setMonth(3);
-//		two.setSpeices("中华田园猫");
-//		two.price = 200;
-		
-		System.out.println(one.getName() + "的售价" + one.price);
-		//System.out.println(two.getName() + "的售价" + two.price);
-		//com.imooc.mechanics.Cat one = new com.imooc.mechanics.Cat();
-		//直接加载com.imooc.animal.CatTest
-		com.imooc.animal.CatTest ct = new com.imooc.animal.CatTest();
+		Cat two = new Cat();
+		//one.run("花花");
+//		one.setName("花花");
+//		one.setMonth(2);
+//		one.setSpeices("英国短毛猫");
+//		//静态成员访问方式：1、对象.成员。2、类.成员	
+//		one.price = 1000;
+//		Cat.price = 2000;
+//		//one.eat();
+//		//Cat.eat();
+//		one.run();
+////		Cat two = new Cat();
+////		two.setName("凯特");
+////		two.setMonth(3);
+////		two.setSpeices("中华田园猫");
+////		two.price = 200;
+//		
+//		System.out.println(one.getName() + "的售价" + one.price);
+//		//System.out.println(two.getName() + "的售价" + two.price);
+//		//com.imooc.mechanics.Cat one = new com.imooc.mechanics.Cat();
+//		//直接加载com.imooc.animal.CatTest
+//		com.imooc.animal.CatTest ct = new com.imooc.animal.CatTest();
 	}
 
 }
