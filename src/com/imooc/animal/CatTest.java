1d5fc0133e9bcbe22a815a37165a924eb4a178ba
gzzwinner <gzzwinner@163.com>
2017-08-07 12:41:51 +0500

    new update

--- src/com/imooc/animal/CatTest.java
+++ src/com/imooc/animal/CatTest.java
@@ -1,22 +1,25 @@
 package com.imooc.animal;
-
+//单一职责原则
 public class CatTest {
 
 	public static void main(String[] args) {
 		//对象实例化
-		Cat one = new Cat();
-		//调用方法
-		one.run();
-		one.eat();
-		one.name = "花花";
-		one.month = 4;
-		one.weight = 1000;
-		one.speices = "英国短毛猫";
-		System.out.println("年龄:" + one.month);
-		System.out.println("姓名:" + one.name);
-		System.out.println("品种:" + one.speices);
-		System.out.println("体重:" + one.weight);
-		one.run(one.name);
+		Cat one = new Cat(3);
+	
+		//one.setName("花花");
+		//one.setMonth(-3);
+//		one.weight = 1000;
+//		one.speices = "英国短毛猫";
+//		
+		if( one.getMonth()==0){
+			return;
+		}
+		System.out.println("年龄:" + one.getMonth());
+		//System.out.println("姓名:" + one.getName());
+//		System.out.println("品种:" + one.speices);
+//		System.out.println("体重:" + one.weight);
+		
+		//one.run();
 	}
 
 }
